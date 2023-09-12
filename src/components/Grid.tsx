dc0db6969b79f3d9baf01fe39c056aea9bd0c604
Mohammed Ali <mohaali482@gmail.com>
2023-09-12 20:13:56 +0300

    Add a method to open numbered grids

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect, useRef } from "react"
 import MemoizedGridSquare from "./GridSquare"
-import DFS from "../utils/algorithm";
+import DFS, { GetNeighbors } from "../utils/algorithm";
 import MineGenerator from "../utils/MineGenerator";
 
 interface GridProps {
@@ -56,15 +56,55 @@ function Grid(props: GridProps) {
         event.preventDefault();
         if (props.gameOver) return;
         const allowableChars = ["F", "U"]
-        if (!allowableChars.includes(props.grid[rowIndex][colIndex])) return false;
 
-        const updatedGrid = [...props.grid];
-        if (updatedGrid[rowIndex][colIndex] == "U") {
-            updatedGrid[rowIndex][colIndex] = "F";
-        } else if (updatedGrid[rowIndex][colIndex] == "F") {
-            updatedGrid[rowIndex][colIndex] = "U";
+
+        if (allowableChars.includes(props.grid[rowIndex][colIndex])) {
+            const updatedGrid = [...props.grid];
+
+            if (updatedGrid[rowIndex][colIndex] == "U") {
+                updatedGrid[rowIndex][colIndex] = "F";
+            } else if (updatedGrid[rowIndex][colIndex] == "F") {
+                updatedGrid[rowIndex][colIndex] = "U";
+            }
+            props.setGrid(updatedGrid);
+        } else if (!isNaN(parseInt(props.grid[rowIndex][colIndex]))) {
+
+            const neighbors = GetNeighbors(props.gridState, rowIndex, colIndex);
+            let flagCounter = 0;
+
+            neighbors.forEach(neighbor => {
+                const [x, y] = neighbor;
+                if (props.grid[x][y] == "F") {
+                    flagCounter++;
+                }
+            });
+
+            if (flagCounter == parseInt(props.grid[rowIndex][colIndex])) {
+                for (let index = 0; index < neighbors.length; index++) {
+                    const updatedGrid = [...props.grid];
+                    const updatedGridState = [...props.gridState];
+                    const [x, y] = neighbors[index];
+                    if (updatedGridState[x][y] == "M" && updatedGrid[x][y] !== "F") {
+                        for (let i = 0; i < updatedGrid.length; i++) {
+                            for (let j = 0; j < updatedGrid[i].length; j++) {
+                                if (updatedGridState[i][j] == "M") {
+                                    updatedGrid[i][j] = "M";
+                                }
+                            }
+                        }
+                        props.setGameOver(true);
+                        props.setStarted(false);
+                        props.setGrid(updatedGrid);
+                        props.setGridState(updatedGridState);
+                    } else if (updatedGrid[x][y] == "U") {
+                        DFS(updatedGridState, updatedGrid, x, y, props.openedGrids)
+                        props.setGrid(updatedGrid);
+                        props.setGridState(updatedGridState);
+                        props.setOpenedGrids({ opened: props.openedGrids.opened });
+                    }
+                }
+            }
         }
-        props.setGrid(updatedGrid);
         return false;
     }
 
