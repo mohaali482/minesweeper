5014568b03c277a07471d6afa357b0ec8dcc14b2
Mohammed Ali <mohaali482@gmail.com>
2023-09-12 18:27:14 +0300

    Added levels

---
+++ src/utils/Levels.ts
@@ -0,0 +1,34 @@
+const Levels = [
+  {
+    level: "iron",
+    gridSizeX: 8,
+    gridSizeY: 8,
+    mines: 10,
+  },
+  {
+    level: "silver",
+    gridSizeX: 16,
+    gridSizeY: 16,
+    mines: 40,
+  },
+  {
+    level: "platinum",
+    gridSizeX: 16,
+    gridSizeY: 30,
+    mines: 99,
+  },
+  {
+    level: "immortal",
+    gridSizeX: 40,
+    gridSizeY: 30,
+    mines: 240,
+  },
+  {
+    level: "radiant",
+    gridSizeX: 100,
+    gridSizeY: 100,
+    mines: 999,
+  },
+];
+
+export default Levels;
